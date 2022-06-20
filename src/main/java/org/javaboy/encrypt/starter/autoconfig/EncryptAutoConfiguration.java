e3e3376bc6ea1ae4156c6cfa442f0be14111a3c1
hudonghui <234498142@qq.com>
2022-06-21 06:29:13 +0800

    1

---
+++ src/main/java/org/javaboy/encrypt/starter/autoconfig/EncryptAutoConfiguration.java
@@ -0,0 +1,11 @@
+package org.javaboy.encrypt.starter.autoconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("org.javaboy.encrypt.starter")
+public class EncryptAutoConfiguration {
+
+}
---
+++ src/main/java/org/javaboy/encrypt/starter/prop/EncryptProperties.java
@@ -0,0 +1,18 @@
+package org.javaboy.encrypt.starter.prop;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix = "spring.encrypt")
+public class EncryptProperties {
+    private final static String DEFAULT_KEY = "www.mark.com";
+    private String key = DEFAULT_KEY;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
